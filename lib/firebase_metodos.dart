687ec2fc2876b1d656b1cba3462f35ffab75727a
VictorM4nu31 <angelesvictor690@gmail.com>
2024-08-13 09:38:10 -0600

    cambios 1.0 version

---
+++ lib/firebase_metodos.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+//Regresa una lista de datos
+Future<List> obtenerDatos() async {
+  List gente = [];
+  CollectionReference collectionReferenceGente = db.collection('001');
+
+  QuerySnapshot queryGente = await collectionReferenceGente.get();
+  queryGente.docs.forEach((documento) {
+    gente.add(documento.data());
+  });
+  //Dar una duracion de 4 segundos para mostrar que el circulo funciona
+  await Future.delayed(const Duration(seconds: 2));
+  return gente;
+}
+
+//Leer datos
+Future<void> leerNombre(String nombre) async {
+  await db.collection("001").add({"nombre": nombre});
+}
+
+//Sintaxis de una base de datos NoSQL de Firebase
+/*
+"bd_usuarios":
+ [
+    {
+      "nombre": "Oscar",
+    },
+    {
+      "nombre": "irene",
+    },
+    {
+      "nombre": "Ulises",
+    },
+    ...
+ ]
+*/
